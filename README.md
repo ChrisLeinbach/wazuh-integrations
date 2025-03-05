96530fabdc8c918d06b730feb98b220586e51adf
ChrisLeinbach <christopher.leinbach@gmail.com>
2025-03-04 19:11:07 -0500

    General Refactor
    - Fix issue with shebang in Python.
    - Clean up comments to match my preferred format.
    - Change alert coloring to match Wazuh UI.
    - Move fields to outer scope in preparation for improvements.

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
-# wazuh-integrations
+# Wazuh Integrations
 
-This repository holds custom wazuh integrations that allow you to forward wazuh alerts to other systems.
+This repository holds a custom Wazuh integration for Discord that allows you to forward Wazuh alerts via Webhook.
 
-**discord** - the discord integration can be used to forward alerts to dedicated discord channels via discord bot
 
-**n8n** - n8n integration lets you handle alerts, e.g. you can create tickets in DFIR-IRIS / TheHive / SecurityOnion / Jira via webhooks/api calls - a custom workflow to combine wazuh and DFIR-IRIS will be uploaded soon
 
 
--- discord/custom-discord.py
+++ discord/custom-discord.py
@@ -1,9 +1,8 @@
-#/usr/bin/env python3
+#!/usr/bin/env python3
 
 import sys
 import requests
 import json
-from requests.auth import HTTPBasicAuth
 
 """
 ossec.conf configuration structure
@@ -14,48 +13,60 @@ ossec.conf configuration structure
  </integration>
 """
 
-# read configuration
+# Read arguments - See https://wazuh.com/blog/how-to-integrate-external-software-using-integrator/
 alert_file = sys.argv[1]
 user = sys.argv[2].split(":")[0]
 hook_url = sys.argv[3]
 
-# read alert file
+# Read JSON data from the alert file.
 with open(alert_file) as f:
     alert_json = json.loads(f.read())
 
-# extract alert fields
+# Extract alert level from the alert.
 alert_level = alert_json["rule"]["level"]
 
-# colors from https://gist.github.com/thomasbnt/b6f455e2c7d743b796917fa3c205f812
-if(alert_level < 5):
-    # green
-    color = "5763719"
-elif(alert_level >= 5 and alert_level <= 7):
-    # yellow
-    color = "16705372"
-else:
-    # red
-    color = "15548997"
-
-# agent details
+# Determine which agent caused the alert.
 if "agentless" in alert_json:
-	  agent_ = "agentless"
+    agent_ = "agentless"
 else:
     agent_ = alert_json["agent"]["name"]
+
+# Colors from https://gist.github.com/thomasbnt/b6f455e2c7d743b796917fa3c205f812
+# Aligned with Wazuh UI color coding.
+if alert_level < 7:
+    # Green
+    color = "5763719"
+elif 7 <= alert_level <= 11:
+    # Blue
+    color = "3447003"
+elif 12 <= alert_level <= 14:
+    # Yellow
+    color = "16705372"
+else:
+    # Red
+    color = "15548997"
+
+fields = [
+    {
+        "name": "Agent",
+        "value": agent_,
+        "inline": True
+    }, {
+        "name": "Level",
+        "value": alert_level,
+        "inline": True
+    }
+]
 
 # combine message details
 payload = json.dumps({
     "content": "",
     "embeds": [
         {
-		    "title": f"Wazuh Alert - Rule {alert_json['rule']['id']}",
-				"color": color,
-				"description": alert_json["rule"]["description"],
-				"fields": [{
-						"name": "Agent",
-						"value": agent_,
-						"inline": True
-						}]
+            "title": f"Wazuh Alert - Rule {alert_json['rule']['id']}",
+            "color": color,
+            "description": alert_json["rule"]["description"],
+            "fields": fields
         }
     ]
 })
