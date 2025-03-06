3754a291b399a97c02ed1a8f1d1baee390666b71
ChrisLeinbach <christopher.leinbach@gmail.com>
2025-03-05 19:07:21 -0500

    Add Install Script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cp -r $SCRIPT_DIR/discord/* /var/ossec/integrations/
+chown -R root:wazuh /var/ossec/integrations/
+chmod -R 750 /var/ossec/integrations/
+systemctl restart wazuh-manager
