0dc29b79a480f8e7024bd8c141f68c10c4c5236a
Christopher Leinbach <christopher.leinbach@gmail.com>
2025-03-14 21:36:16 +0000

    Improve Netstat Handler
    Fixes the formatting on the port strings when the list of ports is empty. Now, instead of a blank line the line will say None.

--- discord/rule_handlers/netstat_ports.py
+++ discord/rule_handlers/netstat_ports.py
@@ -19,9 +19,17 @@ class NetstatPortHandler(BaseHandler):
 
     def generate_description(self) -> Union[str, None]:
         new_lines, removed_lines = self._find_line_deltas(self.alert_data['previous_log'], self.alert_data['full_log'])
-        new_string = f"\nOpened Port(s):\n" + "\n".join(new_lines)
-        old_string = f"\nClosed Port(s):\n" + "\n".join(removed_lines)
+        new_string = f"\nOpened Port(s):\n" + self._format_port_list(new_lines)
+        old_string = f"\nClosed Port(s):\n" + self._format_port_list(removed_lines)
         return f"{new_string}\n{old_string}"
+
+    @staticmethod
+    def _format_port_list(port_list: list[str]) -> str:
+        """ Converts a list of ports into a better formatted string. If the list is empty, converts it to None (str)."""
+        if port_list:
+            return "\n".join(port_list)
+        else:
+            return 'None'
 
     @staticmethod
     def _find_line_deltas(before, after):
