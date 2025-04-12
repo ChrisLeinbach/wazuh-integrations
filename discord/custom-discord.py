eff2c026563458bc5aadc86e6e11f93e3b4b538b
Christopher Leinbach <christopher.leinbach@gmail.com>
2025-04-12 16:35:02 +0000

    Move Ignored Alert IDs to Options
    This commit moves the ignored alert IDs from the Python code base into the options field of the Wazuh integration. This makes the code less opinionated about how a user should have things configured and prevents Git operations from overwriting a user's ignored alert IDs.

--- discord/custom-discord.py
+++ discord/custom-discord.py
@@ -21,12 +21,11 @@ LOG_HIDE_WEBHOOK_URL = False
 
 logging.basicConfig(level=LOG_LEVEL, filename=LOG_FILE, filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
 
-IGNORED_ALERT_IDS = ['23504', '23505']
-
 # Read arguments - See https://wazuh.com/blog/how-to-integrate-external-software-using-integrator/
 alert_file = sys.argv[1]
 user = sys.argv[2].split(":")[0]
 hook_url = sys.argv[3]
+options_file = sys.argv[5]
 
 logging.debug(f'Custom-Discord Initialized. '
               f'Alert file: {alert_file}, '
@@ -38,6 +37,24 @@ logging.debug(f'Loaded Handlers: {[cls.__name__ for cls in rule_handlers.get_all
 # Read JSON data from the alert file.
 with open(alert_file) as f:
     alert_json = json.loads(f.read())
+
+# Read JSON Data from the options file.
+# Need to handle invalid JSON here. Wazuh does not validate the user's options so we can receive invalid options data.
+# Also need to handle not having options at all. In this case, we just make it an empty dict.
+if options_file:
+    with open(options_file) as f:
+        try:
+            options_json = json.loads(f.read())
+            logging.debug(f"Provided Options: {json.dumps(options_json)}")
+        except json.decoder.JSONDecodeError:
+            logging.critical(f'Failed to read integration options data. Check integration configuration in Wazuh GUI.')
+            sys.exit(1)
+else:
+    options_json = {}
+
+# Read options. This may require a more intelligent implementation if we add more options but for now this is
+# an acceptable approach.
+IGNORED_ALERT_IDS = options_json.get('ignored_alert_ids', [])
 
 # Extract alert level and ID from the alert.
 alert_id = alert_json['rule']['id']
