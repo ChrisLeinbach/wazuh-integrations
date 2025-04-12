dea740318abcc704a03f7f05329854ed4a53a1a9
Christopher Leinbach <christopher.leinbach@gmail.com>
2025-04-12 17:09:05 +0000

    Add PAM Handler
    Adds a handler for PAM events. IDs for these are 5501 and 5502.
    
    These are generated on Linux login/sudo/logout events.

---
+++ discord/rule_handlers/pam.py
@@ -0,0 +1,43 @@
+from typing import List, Union
+from copy import deepcopy
+from rule_handlers.base_handler import BaseHandler
+
+
+class PAMHandler(BaseHandler):
+
+    """
+    PAMHandler handles alerts where events from PAM, the Linux authentication manager.
+    """
+
+    alert_ids = ['5501', '5502']
+    enabled = True
+
+    def __init__(self, alert_data: dict):
+        super().__init__(alert_data)
+
+    def generate_fields(self) -> List[dict]:
+        fields = []
+
+        if 'srcuser' in self.alert_data['data'].keys():
+            src_user_field = deepcopy(self.base_field)
+            src_user_field["name"] = "Source User"
+            src_user_field["value"] = self.alert_data['data']["srcuser"]
+            fields.append(src_user_field)
+
+        if 'uid' in self.alert_data['data'].keys():
+            uid_field = deepcopy(self.base_field)
+            uid_field["name"] = "UID"
+            uid_field["value"] = self.alert_data['data']["uid"]
+            fields.append(uid_field)
+
+        if 'dstuser' in self.alert_data['data'].keys():
+            dst_user_field = deepcopy(self.base_field)
+            dst_user_field["name"] = "Destination User"
+            dst_user_field["value"] = self.alert_data['data']["dstuser"]
+            fields.append(dst_user_field)
+
+        return fields
+
+    def generate_description(self) -> Union[str, None]:
+        """ Default description is acceptably specific. """
+        return None
